44f354cb2926e6b67c8ccbee761a7cad8ec57cd8
cedric klaucke <cedricklaucke@gmail.com>
2020-11-06 15:36:45 +0100

    V2.2 added text and language option

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+// code for the sidebar
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("content-wrap").style.marginRight = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginRight= "0";
+  document.getElementById("content-wrap").style.marginRight= "0";
+}
+
+// code for the dropdown
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
