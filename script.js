3ea508986e52dd3d2cda822b5e5c4055559d6e6c
cedric klaucke <cedricklaucke@gmail.com>
2021-04-02 02:52:11 +0200

    V2.4.4 Added Databases content

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ function sideDropdownbtn() {
 }
 
 window.addEventListener("click", function(event) {
-  this.classList.toggle("active");
   if (!event.target.matches('.dropdown-btn')) {
     var dropdowns = document.getElementsByClassName("side-dropdown");
     var i;
@@ -47,5 +46,23 @@ window.addEventListener("click", function(event) {
         openDropdown.classList.remove('show');
       }
     }
+  }
+});
+
+// Opens and closes the databases dropdowns
+document.addEventListener("DOMContentLoaded", function(){
+  var coll = document.getElementsByClassName("databases-box");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
   }
 });
